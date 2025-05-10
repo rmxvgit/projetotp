1a7ee92aac814b37b8ad9bb7f963d490197e4e3c
Pedro Souza Batista <pedrosouzab.p75@gmail.com>
2025-05-10 20:26:04 -0300

    defining parameters to test name as const

--- dominios/name/name_teste.hpp
+++ dominios/name/name_teste.hpp
@@ -4,16 +4,19 @@
 using namespace std;
 class testeDeFumacaNome {
     private:
-        string nomeComComprimentoValido = "Eren Jaeger";
-        string nomeComComprimentoInvalido = "Roronoa Zoro no Ichibansenpai";
-        string nomeComCaracteresValidos = "99 Edward Elric";
-        string nomeComCaracteresInvalidos = "Ichig@ Kurosak!";
-        string nomeComEspacamentoValido = "S a t o r u G o j o";
-        string nomeComEspacamentoInvalido = "Tanjiro  Kamado";
+       const string nomeComComprimentoValido = "Eren Jaeger";
+       const string nomeComComprimentoInvalido = "Roronoa Zoro no Ichibansenpai";
+       const string nomeComCaracteresValidos = "99 Edward Elric";
+       const string nomeComCaracteresInvalidos = "Ichig@ Kurosak!";
+       const string nomeComEspacamentoValido = "S a t o r u G o j o";
+       const string nomeComEspacamentoInvalido = "Tanjiro  Kamado";
+
         Name *nome;
         int estado;
+
         void configurarNome();
         void deletarNome();
+        
         void testarNomeComComprimentoValido();
         void testarNomeComComprimentoInvalido();
         void testarNomeComCaracteresValidos();
