a96c054f63e3ef1d0eeade4a8f799bfb269ba79c
Pedro Souza Batista <pedrosouzab.p75@gmail.com>
2025-05-14 05:59:37 -0300

    rebase name_test.hpp

---
+++ dominios/name/name_test.hpp
@@ -0,0 +1,26 @@
+#include "name.hpp"
+
+using namespace std;
+
+class SmokeTestName {
+    private:
+       const string validName = "Eren Jaeger";
+       const string invalidLengthName = "Roronoa Zoro no Ichibansenpai";
+       const string invalidCharacterName = "Ichig@ Kurosak!";
+       const string invalidSpacingName = "Tanjiro  Kamado";
+
+       Name *nameInstance;
+       bool testStatus;
+
+       void setUp();
+       void tearDown();
+
+       void testValidName();
+       void testInvalidLength();
+       void testInvalidCharacter();
+       void testInvalidSpacing();
+
+    public:
+
+       bool run();
+};
