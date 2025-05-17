869524dc766d8ca5db17d92f5493c44d56a4455b
Pedro Souza Batista <pedrosouzab.p75@gmail.com>
2025-05-17 09:22:39 -0300

    documentacao do dominio name

--- dominios/name/name.hpp
+++ dominios/name/name.hpp
@@ -10,6 +10,22 @@
 
 using namespace std;
 
+/**
+ * @brief Identificação única de um usuário em um determinado sistema, plataforma ou serviço online
+ *
+ *  Regras de formato:
+ *
+ * - A string que representa o nome do usuário não pode ter mais de 20 caracteres.
+ *
+ * - Cada caracter pode ser dígito de 0 à 9), letra (A – Z ou a – z) ou espaço em branco.
+ * 
+ * - Não é permitido pontuação como vírgula, pontos, acentos, sinais gráficos e etc...
+ *
+ * - Não pode existir uma seguência de espaços em branco.
+ * 
+ * 
+ */
+
 class Name{
     private :
         static const int MAXIMUM_LENGTH = 20;
@@ -21,10 +37,28 @@ class Name{
         bool validator(const string &);
 
     public:
-
+        /**
+         * @brief Inicializa o objeto com ""
+         */
         Name();
+
+        /**
+ * @brief Construtor que inicializa o objeto com o nome informado, caso seja válido.
+ *
+ * Este construtor valida o nome fornecido de acordo com as regras estabelecidas.
+ *
+ * @param name Nome de usuário a ser atribuído ao objeto.
+ *
+ * @throw invalid_argument("The user's name cannot contain more than 20 characters.")
+ * @throw invalid_argument("The user's name cannot have consecutive spaces.")
+ * @throw invalid_argument("The user's name cannot contain special characters.")
+ */
         Name(const string &);
 
+        /**
+         * @brief Retorna o nome informado.
+         * @return name.
+         */
         string Get();
         void Set(const string &);
 };
